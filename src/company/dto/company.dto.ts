cb3d56dd58c44b1e701e012c4e49ab25ebf6c45d
sahriar nur <shariarnurnahin99@gmail.com>
2026-09-18 06:57:38 -0600

    updates company module with additional fields

--- src/company/dto/company.dto.ts
+++ src/company/dto/company.dto.ts
@@ -1,16 +1,27 @@
 import { createZodDto } from "nestjs-zod";
 import { z } from "zod";
 
-import { SHORT_LENGTH, requiredStr } from "@/src/common/validation";
+import { SHORT_LENGTH, requiredStr, str } from "@/src/common/validation";
 
-const createSchema = z.object({
+const companySchema = z.object({
   name: requiredStr(SHORT_LENGTH),
+  aliases: z.array(str(SHORT_LENGTH)).optional(),
+  links: z
+    .array(
+      z.object({
+        type: str(SHORT_LENGTH),
+        url: z.url(),
+      }),
+    )
+    .optional(),
+  careerPageUrl: z.url().nullish(),
+  researchDossier: z.unknown().optional(),
 });
 
-export class CreateCompanyDto extends createZodDto(createSchema) {}
+export class CreateCompanyDto extends createZodDto(companySchema) {}
 
-const updateSchema = z.object({
-  name: requiredStr(SHORT_LENGTH).optional(),
-});
-
-export class UpdateCompanyDto extends createZodDto(updateSchema) {}
+export class UpdateCompanyDto extends createZodDto(
+  companySchema.partial().extend({
+    name: requiredStr(SHORT_LENGTH).optional(),
+  }),
+) {}
